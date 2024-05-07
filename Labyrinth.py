1e375bca546b56f4ab3a7da24bcd057bd11813e9
iceman <jeremi.toroj@icloud.com>
2024-05-07 11:36:18 +0200

    Labyrinth class file

--- Labyrinth.py
+++ Labyrinth.py
@@ -1,1 +1,28 @@
-/Labyrinth.py
+class Mase:
+
+    def __init__(self, *args):
+        """
+        Constructor for the Maze class, initializes the width and height of the maze
+        :param args: width and height of the maze
+        """
+        if len(args) == 1:
+            self.width = args[0]
+            self.height = args[0]
+        elif len(args) == 2:
+            self.width = args[0]
+            self.height = args[1]
+        elif len(args) == 0:
+            self.width = 10
+            self.height = 10
+        else:
+            raise ValueError("Invalid number of arguments")
+
+    def generate(self):
+        """
+        Generates the maze using the <fill> algorithm
+        :return: maze as a 2D list
+        """
+        pass
+
+    def display(self):
+        pass
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Labyrinth
+### Jeremi Torój 
+#### 7.05.2024
+
 Python class creating labyrinth
+
+## Functions
+### Constructors
+Maze() # Create a maze object with dicfault size of 10x10 
+Maze(10) # Creates a square maze object with a size of 10x10  
+Maze(10, 20) # Creates a maze object with a size of 10x20
+
+### Methods
+generate() # Generates a maze  
+display() # Displays the maze
