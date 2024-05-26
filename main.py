a4e2c53d61bd49a65a96f11f9bbc311c95cd896b
Jeremi Torój <jeremi.toroj@icloud.com>
2024-05-26 14:43:51 +0200

    text box for maze width and height

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import pygame
 from Labyrinth import Maze
+import tkinter as tk
 
 
-maze = Maze(10)
+def get_maze_dimensions():
+    dimensions = {'width': 0, 'height': 0}
+
+    def submit():
+        dimensions['width'] = int(width_entry.get())
+        dimensions['height'] = int(height_entry.get())
+        root.destroy()
+
+    root = tk.Tk()
+    tk.Label(root, text="Maze width:").grid(row=0)
+    tk.Label(root, text="Maze height:").grid(row=1)
+
+    width_entry = tk.Entry(root)
+    height_entry = tk.Entry(root)
+
+    width_entry.grid(row=0, column=1)
+    height_entry.grid(row=1, column=1)
+
+    tk.Button(root, text='Submit', command=submit).grid(row=3, column=0, sticky=tk.W, pady=4)
+
+    tk.mainloop()
+
+    return dimensions['width'], dimensions['height']
+
+
+width, height = get_maze_dimensions()
+maze = Maze(width, height)
 
 running = True
 while running:
